4a27748971a8de2173c9242489d95b9828da1cc4
ma-ra-bu <daniel.engler@gyminterlaken.ch>
2017-08-02 12:57:50 +0200

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,13 +1,31 @@
 var x = document.getElementById("demo");
+var btn = document.getElementById("btnStart");
+var watchID = null;
 
-function getLocation() {
+function startGPS() {
     if (navigator.geolocation) {
-        navigator.geolocation.watchPosition(showPosition);
+        watchID=navigator.geolocation.watchPosition(showPosition);
+        btn.disabled = true;
     } else { 
-        x.innerHTML = "Geolocation is not supported by this browser.";}
+        x.innerHTML = "Geolocation is not supported by this browser.";
     }
+}
     
 function showPosition(position) {
-    x.innerHTML="Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude;
+	 var jetzt = new Date();
+	 jetzt.setTime(position.timestamp);
+    x.innerHTML='Latitude: '          + position.coords.latitude          + '<br>' +
+          'Longitude: '         + position.coords.longitude         + '<br>' +
+          'Altitude: '          + position.coords.altitude          + '<br>' +
+          'Accuracy: '          + position.coords.accuracy          + '<br>' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '<br>' +
+          'Heading: '           + position.coords.heading           + '<br>' +
+          'Speed: '             + position.coords.speed             + '<br>' +
+          'Time: '              + jetzt.toLocaleString('de-DE');
+}
+
+function stopGPS() {
+	navigator.geolocation.clearWatch(watchID);
+	x.innerHTML = "Ende";
+	btn.disabled = false;
 }
